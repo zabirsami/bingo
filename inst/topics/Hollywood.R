226170add5b68d77325b21ec641792d7e01b4a57
zabirsami <zmahmud@ucmerced.edu>
2021-04-22 00:06:29 -0700

    Add Hollywood
    Adding Hollywood to the github bingo branch

---
+++ inst/topics/Hollywood.R
@@ -0,0 +1,29 @@
+# Hollywood
+
+c(
+  "The Godfather",
+  "The Abyss",
+  "Aliens",
+  "Emma Stone",
+  "George Clooney",
+  "Psycho",
+  "Viola Davis",
+  "One Flew Over The Cuckoos Nest",
+  "Quentin Terantino",
+  "Guess Who's Coming to Dinner",
+  "Wicked",
+  "Les Miserables",
+  "FREE!",
+  "West Side Story",
+  "The Imitation of Life",
+  "James Cameron",
+  "Leonardo DiCaprio",
+  "Will Smith",
+  "Rita Moreno",
+  "Katherine Hepburn",
+  "Bette Davis",
+  "Grease",
+  "Life of Pi",
+  "Jaws",
+  "Spike Lee"
+)
